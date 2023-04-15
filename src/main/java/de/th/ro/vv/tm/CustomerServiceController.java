680d8cc00902be54c2a7c570fb4140b4a4abb23b
Thomas Mildner <kontakt@mildner-thomas.de>
2023-04-15 20:55:17 +0200

    - renamed customer controller - renamed docker images - removed docker build, use native docker images instead

--- src/main/java/de/th/ro/vv/tm/CustomerServiceController.java
+++ src/main/java/de/th/ro/vv/tm/CustomerServiceController.java
@@ -3,7 +3,7 @@ package de.th.ro.vv.tm;
 import io.micronaut.http.annotation.*;
 
 @Controller("/customerservice")
-public class CustomerserviceController {
+public class CustomerServiceController {
 
     @Get(uri="/", produces="text/plain")
     public String index() {
